e0745fcb62fa3217b428e7f4c614c506588d1129
korobool <korobov.alex@gmail.com>
2013-07-14 21:57:45 +0300

    Test coordinates compensation.

--- src/com/example/mmaper/MainActivity.java
+++ src/com/example/mmaper/MainActivity.java
@@ -28,8 +28,8 @@ public class MainActivity extends Activity {
     {
         x = e.getX();
         y = e.getY();
-        GetterSetter.LastX = x;
-        GetterSetter.LastY = y;
+        GetterSetter.LastX = x - 10;
+        GetterSetter.LastY = y - 100;
         return true;
     }
 
